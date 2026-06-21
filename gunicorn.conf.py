cfad510c5368b1c67f68179684795c04f9a271c4
Mirandateresa <maritemirandavega@gail.com>
2026-06-21 16:57:03 -0600

    Optimizar: Desactivar Hill Climbing y reducir timeouts

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -2,8 +2,8 @@
 import os
 
 bind = "0.0.0.0:" + os.environ.get("PORT", "10000")
-workers = 2
-threads = 2
+workers = 1
+threads = 4
 timeout = 180  # Aumentado a 180 segundos (3 minutos)
 graceful_timeout = 60
 worker_class = "sync"
